9f88884e4521c9fea78652c16929e089ee316f6d
Veronika-Kot <Veronika-Kot@Veronikas-MBP.att.net>
2016-03-05 15:15:00 -0800

    Merging

---
+++ iStream1.1/MediumMenu-Sample/MediumMenu-Sample/PhotosCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PhotosCollectionViewCell.swift
+//  iStream
+//
+//  Created by Veronika Kotckovich on 3/5/16.
+//  Copyright © 2016 pixyzehn. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageViewer: UIImageView!
+    
+}
--- iStream1.1/MediumMenu-Sample/MediumMenu-Sample/View/StreamViewController.swift
+++ iStream1.1/MediumMenu-Sample/MediumMenu-Sample/View/StreamViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class StreamViewController: BaseViewController {
+class StreamViewController: BaseViewController, UITableViewDataSource, UITableViewDelegate {
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
     }
@@ -17,7 +17,84 @@ class StreamViewController: BaseViewController {
         super.init(coder: aDecoder)
     }
     
+    @IBOutlet weak var tableView: UITableView!
+    var streams = ["1", "2"]
+    var numCell: Int = 2
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+        // Do any additional setup after loading the view.
+        
+        
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(named: "icon"), style: UIBarButtonItemStyle.Plain, target: self, action: Selector("onAddingStream:"))
     }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return streams.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("StreamCell", forIndexPath: indexPath) //as! StreamViewCell
+        print("row\(indexPath.row)")
+        return cell
+    }
+    
+    //DELETE a cell
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        print("On deletion")
+        if editingStyle == UITableViewCellEditingStyle.Delete {
+            streams.removeAtIndex(indexPath.row)
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Automatic)
+            numCell--
+            
+        }
+    }
+    
+    func onAddingStream(sender: UIBarButtonItem) {
+        
+        print("on editing")
+        if (numCell < 3) {
+            numCell++
+            streams.append("new stream")
+            //tableView.frame = CGRectMake(tableView.frame.origin.x, tableView.frame.origin.y, tableView.frame.size.width, tableView.contentSize.height + 250)
+            tableView.reloadData()
+        }
+        else if(numCell == 5) {
+            let alertController = UIAlertController(title: "iStream", message:
+                "You have to much sterams! Save one to create a new", preferredStyle: UIAlertControllerStyle.Alert)
+            alertController.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default,handler: nil))
+            
+            self.presentViewController(alertController, animated: true, completion: nil)
+            print("Promt: You have to much sterams! Save one to create a new")
+        }
+        else{
+            streams.append("new stream")
+            numCell++
+            tableView.reloadData()
+        }
+        //
+        //            //tableView.contentSize = CGSize(width: tableView.frame.size.width, height: tableView.frame.size.height + 179)
+        print("on editing")
+        //}
+        
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "streamsToDetail" {
+            let cell = sender as! UITableViewCell
+            let indexPath = tableView.indexPathForCell(cell)
+            let detailViewController = segue.destinationViewController as! DetailViewController
+        }
+    }
+
+    
 }
