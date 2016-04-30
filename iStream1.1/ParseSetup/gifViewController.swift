cf999fe71d1a0ecc08d32b44aaffa2ddf30f1df2
Veronika-Kot <Veronika-Kot@Veronikas-MBP.att.net>
2016-04-29 21:06:22 -0700

    last commit

--- iStream1.1/ParseSetup/gifViewController.swift
+++ iStream1.1/ParseSetup/gifViewController.swift
@@ -370,7 +370,7 @@ class gifViewController: UIViewController {
         let imageAnimator = ImageAnimator(renderSettings: settings)
         imageAnimator.render() {
             print("Video created!")
-            let alertController = UIAlertController(title: "Default Style", message: "Yoohoo! Your video was created! Check your library!", preferredStyle: .Alert)
+            let alertController = UIAlertController(title: "Congrats!", message: "Yoohoo! Your video was created! Check your library!", preferredStyle: .Alert)
             let OKAction = UIAlertAction(title: "OK", style: .Default) { (action) in
             }
             alertController.addAction(OKAction)
